01e550bc1ab9d6917ef0e14f093652fd7fb2b718
mircea <mircea_us@live.com>
2025-04-18 10:15:14 +0300

    added code to process the RX SSI data of encoder value

--- sw/AEAT6600ConfigAndReadData/app/uart/u.c
+++ sw/AEAT6600ConfigAndReadData/app/uart/u.c
@@ -1,25 +1,68 @@
 #include "u.h"
 #include "main.h"
 
-extern UART_HandleTypeDef huart3;
 
-// Define a structure for menu items
+static void setConditionsFor(uint8_t modeOtpOrReadEnc);
+static void getEncVal( void );
+static void setOTPField( void );
+static void getOTPVal( void );
+static void programOTPData( void);
+
+
+#define NB_OF_CLK_EDGES 71
+#define NB_OF_OTP_ELEMS 12
+
+
 typedef struct {
     char *optionName;
     char *helpText;
     void (*action)(void);
 } MenuItem;
 
-#define NB_OF_CLK_EDGES 71
-
 enum
 {
 	eProgOTP,
-	eReadEncVal
+	eReadEncVal,
+	eReadOTPVal
 };
 
+typedef enum
+{
+	eInit = 0,
+	eWaitRx,
+	eWaitChoiceActions,
+	eWaitSSITransfer,
+	eWaitTxEnd
+}uStates;
+
+
+extern UART_HandleTypeDef huart3;
+extern TIM_HandleTypeDef htim1;
+
+
+static uint8_t uartRxBuf[10];
+static uint8_t encResolution[]={10u,12u,14u,16u};
+static uint8_t menuSize;
+static uint8_t mPrompt[]="cmd:";
+static uint8_t uartMenu[1024];
+static uint8_t uState;
+static uint8_t fLedMagHiToggled;
+static uint8_t fLedMagLoToggled;
+static uint8_t ssiMode;
+static uint8_t *cOtpClk;
+static uint8_t *cOtpData;
+static uint8_t ssiClkEdgesNb;
+static uint8_t fUARTRx;
+static uint8_t fWaitSSITransfer;
+static uint8_t fParseSSIRxData;
+
+static uint16_t encVal;
+
+static uint32_t tim1Tick;
+static uint32_t cntWaitOTPWriteStatusFlags;
+
 //values for activating the ABI interface
-uint8_t otpData[2][2][NB_OF_CLK_EDGES+2] =
+static uint8_t otpData[2][2][NB_OF_CLK_EDGES+2] =
  {
   /*read*/
   {/*CLK*/ {1,0, /*RD/WR*/1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, 1,0,1,0,1,0,1,0, /*par*/1, 0, 1, 1, 1 },
@@ -31,12 +74,9 @@ uint8_t otpData[2][2][NB_OF_CLK_EDGES+2] =
 
  };
 
-
-#define nbOfElems 12
-
-uint8_t OTPData[32]={0,}; //default values: all bits 0
-uint8_t OTPDataBitPos[nbOfElems]={0,3,5,7,8,10,11,12,13,14,15,16};
-uint8_t menuOTPDescr[]=
+static uint8_t OTPData[32]={0,}; //default values: all bits 0
+static uint8_t OTPDataBitPos[NB_OF_OTP_ELEMS]={0,3,5,7,8,10,11,12,13,14,15,16};
+static uint8_t menuOTPDescr[]=
 "0 1 2  3 4   56  7  8 9   10 11 12 13 14 15  16          ..                31  \r\n\
 0 0 0  0 0   ..  0  0 0   0  0  0  0  0  0   0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0	\r\n\
 |      |     |   |  |     |  |  |  |  |  |   | 									\r\n\
@@ -53,76 +93,27 @@ uint8_t menuOTPDescr[]=
 |      [3,4] modeLo.IndexPulseWidth = 0											\r\n\
 [0..2] modeLo.IncResolution = 0													\r\n";
 
-
-void setConditionsFor(uint8_t modeOtpOrReadEnc);
-void getEncVal( void );
-void setOTPField( void );
-void getOTPVal( void );
-void programOTPData( void);
-
 // Define menu items
-MenuItem menu[] = {
+static MenuItem menu[] = {
     {"ge", "g - get encoder value", getEncVal},
 	{"go", "o - get current OTP data", getOTPVal},
     {"so,", "s,<pos>,<0xval> - set OTP field; replace <pos> with the field bit position and <0xval> with the desired field value in hex", setOTPField},
     {"po", "p - program the OTP data", programOTPData}
 };
 
-uint8_t uartRxBuf[10];
-
-
-uint8_t encResolution[]={10u,12u,14u,16u};
-
-uint8_t menuSize;
-
-uint8_t mPrompt[]="cmd:";
-
-uint8_t uartMenu[1024];
-
-typedef enum
-{
-	eInit = 0,
-	eWaitRx,
-	eWaitChoiceActions,
-	eWaitTxEnd
-}uStates;
-
-uint8_t uState;
-
-uint8_t fLedMagHiToggled;
-uint8_t fLedMagLoToggled;
-
-
-uint8_t ssiMode;
-
-uint8_t *cOtpClk;
-uint8_t *cOtpData;
-
-uint8_t ssiClkEdgesNb;
-
-
-uint8_t fUARTRx;
-uint8_t fWaitSSITransfer;
-
-uint32_t tim1Tick;
-
-uint32_t cntWaitOTPWriteStatusFlags;
-
-extern TIM_HandleTypeDef htim1;
-
-void setConditionsFor(uint8_t modeOtpOrReadEnc)
+static void setConditionsFor(uint8_t modeOtpOrReadEnc)
 {
 	if ( eProgOTP == modeOtpOrReadEnc )
 	{
-		//TODO - enable 7V output to VPP pin; ALIGN, PWRDOWN – Set Low, PROG – Set High
+		//TODO - enable 6.5V output to VPP pin; ALIGN, PWRDOWN – Set Low, PROG – Set High
 	}
 	else
 	{
-		//TODO - disable 7V output to VPP pin; ALIGN, PWRDOWN – Set Low, PROG – Set Low
+		//TODO - disable 6.5V output to VPP pin; ALIGN, PWRDOWN – Set Low, PROG – Set Low
 	}
 }
 
-void getEncVal( void )
+static void getEncVal( void )
 {
 	uint8_t encResolutionOTPVal = (OTPData[9]<<1) + OTPData[8]; //bits 9 and 8 contain the resolution configuration
 	setConditionsFor(eReadEncVal);
@@ -133,13 +124,14 @@ void getEncVal( void )
 	ssiClkEdgesNb = encResolution[encResolutionOTPVal]*2u;
 
 	fWaitSSITransfer  = 1u;
+	fParseSSIRxData = 1u;
 
 	HAL_GPIO_WritePin(GPIOA, ssiNCS_Pin, GPIO_PIN_RESET);
 	HAL_TIM_Base_Start_IT(&htim1);
 
 }
 
-void getOTPVal( void )
+static void getOTPVal( void )
 {
 	setConditionsFor(eReadEncVal);
 	ssiMode = read;
@@ -154,13 +146,13 @@ void getOTPVal( void )
 	HAL_TIM_Base_Start_IT(&htim1);
 }
 
-void setOTPField( void )
+static void setOTPField( void )
 {
 	//TODO - check the value passed after option name: so and update the otpData[1] buffer and menuOTPDescr with the values
 
 }
 
-void programOTPData( void)
+static void programOTPData( void)
 {
 	setConditionsFor(eProgOTP);
 	ssiMode = write;
@@ -176,12 +168,33 @@ void programOTPData( void)
 	HAL_TIM_Base_Start_IT(&htim1);
 }
 
-uint16_t prepareMenu( void ) {
+static uint16_t prepareMenu( void ) {
 	uint16_t lIdx = 0u;
 	uint16_t lIdxuMenu = 0u;
 	uint8_t lMenuItem = 0u;
+	uint8_t encValDescr[] = "Encoder value: ";
+	uint8_t encValTextRev[] = "65535";
+	uint16_t lencVal = encVal;
 
-	//TODO copy Encoder value data
+	//copy encoder value
+	for (/*lIdx already initialized above*/;lIdxuMenu<(sizeof(encValDescr)-1u); lIdxuMenu++)
+	{
+		uartMenu[lIdxuMenu] = encValDescr[lIdxuMenu];
+	}
+	//get digits
+	encValTextRev[0] = encVal % 10u;
+	lIdx = 1u;
+	while (lencVal != 0u)
+	{
+		encValTextRev[lIdx++] = (lencVal / 10u) % 10u ;
+	}
+	//put encoder value digits in the buffer (in correct order from most significant digit to less significant one)
+	while ( lIdx > 0u )
+	{
+		uartMenu[lIdxuMenu++] = encValTextRev[lIdx--];
+	}
+	uartMenu[lIdxuMenu++] = '\r';
+	uartMenu[lIdxuMenu++] = '\n';
 
 	//TODO update actual OTP data according to the received data from device via SSI interface
 	//TODO update the preset OTP data according to the values set via UART interface
@@ -211,7 +224,7 @@ uint16_t prepareMenu( void ) {
 
 }
 
-void processChoice(uint8_t choice[]) {
+static void processChoice(uint8_t choice[]) {
 	uint8_t lIdx = 0u;
 	uint8_t lMenuItem = 0u;
 	uint8_t lFound = 0u;
@@ -234,12 +247,30 @@ void processChoice(uint8_t choice[]) {
 	}
 }
 
+static void processSSIRxData( void )
+{
+	uint8_t lIdx = 0u;
+	uint8_t bitNb = 0u;
+	uint16_t lencVal = 0u;
+
+	for (lIdx=0u;lIdx<ssiClkEdgesNb;lIdx+=2u)
+	{
+		lencVal |= cOtpData[lIdx]<<bitNb; //cOtpData has only 0's and 1's
+		bitNb++;
+	}
+	encVal = lencVal;
+}
+
+
+
 void uInit( void )
 {
 	menuSize = 4u;
 	uState = eInit;
 
 	fWaitSSITransfer = 0u;
+	fParseSSIRxData = 0u;
+
 	ssiMode = read;
 	ssiClkEdgesNb = 2u*32u;
 	fUARTRx = 0u;
@@ -264,19 +295,32 @@ void uTask( void ) {
 		{
 			fUARTRx = 0u;
 			processChoice(uartRxBuf);
-
-			uState=eWaitChoiceActions;
+			if ( 0u == fWaitSSITransfer )
+			{//command does not start SSI transfer
+				uState=eWaitChoiceActions;
+			}
+			else
+			{//SSI transfer started
+				uState = eWaitSSITransfer;
+			}
 		}
 		break;
-	case eWaitChoiceActions:
+	case eWaitSSITransfer:
 		if ( 0u == fWaitSSITransfer )
 		{
-			//TODO MiPa process SSI data if any - and prepare the menu accordingly
-			uartDataSize = prepareMenu();
-			HAL_UART_Transmit_DMA(&huart3, uartMenu, uartDataSize);
+			if ( 0u != fParseSSIRxData )
+			{
+				processSSIRxData();
+			}
+			uState=eWaitChoiceActions;
+		}//else: wait some more
+		break;
+	case eWaitChoiceActions:
 
-			uState=eWaitTxEnd;
-		}
+		uartDataSize = prepareMenu();
+		HAL_UART_Transmit_DMA(&huart3, uartMenu, uartDataSize);
+
+		uState=eWaitTxEnd;
 		break;
 	case eWaitTxEnd:
 		//TODO if ( )
@@ -297,7 +341,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 	}
 	else
 	{
-		cOtpData[tim1Tick] = HAL_GPIO_ReadPin(GPIOA, ssiDO_Pin);
+		cOtpData[tim1Tick] = HAL_GPIO_ReadPin(GPIOA, ssiDO_Pin); //read on every edge but later use only the data sampled on falling edge
 	}
 	HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin); //led shows the TX status
 
